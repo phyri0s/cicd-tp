f81dcccab879703b0ba52c740b6fee677d8e497c
Phyrios <dev@phyrios.fr>
2025-12-10 12:23:42 +0100

    new try

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,5 @@ module.exports = {
   testEnvironment: 'node',
   collectCoverageFrom: ['src/**/*.js'],
   coveragePathIgnorePatterns: ['/node_modules/'],
-  testPathIgnorePatterns: ['/node_modules/'],
-  reporters: [
-    'default',
-    [
-      'jest-json-reporter',
-      {
-        outputFile: 'test-results.json'
-      }
-    ]
-  ]
+  testPathIgnorePatterns: ['/node_modules/']
 };
