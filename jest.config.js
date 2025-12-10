253b95199bf06efffb22a0a8e7b57b4e6a144bc3
Phyrios <dev@phyrios.fr>
2025-12-10 12:11:43 +0100

    nt

--- jest.config.js
+++ jest.config.js
@@ -2,20 +2,5 @@ module.exports = {
   testEnvironment: 'node',
   collectCoverageFrom: ['src/**/*.js'],
   coveragePathIgnorePatterns: ['/node_modules/'],
-  testPathIgnorePatterns: ['/node_modules/'],
-  reporters: [
-    'default',
-    [
-      'jest-allure',
-      {
-        resultsDir: 'allure-results',
-        testCase: {
-          labels: {
-            epic: 'My Project',
-            story: 'Tests'
-          }
-        }
-      }
-    ]
-  ]
+  testPathIgnorePatterns: ['/node_modules/']
 };
